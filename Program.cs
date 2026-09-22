5b0edb95c020ba91cdda0aef17e18745a47aaf00
ds0036530-sketch <ds0036530@gmail.com>
2026-09-22 10:59:55 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace program
+
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("ingrese un numero:");
+
+            int numero = Convert.ToInt32(Console.ReadLine());
+
+            if (numero % 2 == 0)
+            {
+                Console.WriteLine("El numero es par.");
+            }else
+            {
+                Console.WriteLine("el numero es impar.");
+            }
+
+    
+            }
+        }
+    }
